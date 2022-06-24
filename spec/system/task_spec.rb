22bbcdfcc52826b4028d8ed25a9141ab44e564c4
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-24 18:12:27 +0900

    made Rspec test about order of index

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 RSpec.describe 'The function of task management', type: :system do
-
-  before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-  end
-
   describe 'the function of to create task' do
     context 'creating task' do
       it 'is shown at index' do
@@ -15,15 +9,31 @@ RSpec.describe 'The function of task management', type: :system do
       end
     end
   end
+
   describe 'to display index.html' do
+
+    before do
+      FactoryBot.create(:task)
+      FactoryBot.create(:second_task)
+      visit tasks_path
+    end
+
     context 'visiting index page' do
       it 'is shown all created tasks' do
-        visit tasks_path
         expect(page).to have_content 'title 1'
         expect(page).to have_content 'title 2'
       end
     end
+
+    context 'tasks are arranged in descending created order' do
+      it 'is shown newest task on top' do
+        task_list = all('.task_row')
+        expect(task_list[0]). to have_content('title 2')
+        expect(task_list[1]).to have_content('title 1')
+      end
+    end
   end
+
   describe 'to display show.html' do
     context 'visiting one page' do
       it 'is shown that page' do
