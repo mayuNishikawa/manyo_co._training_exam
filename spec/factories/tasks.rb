2a867a2745e2a5f9367d25d9b9fd852084e68848
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-28 08:18:06 +0900

    made Rspec test about order of deadline

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,19 @@
 FactoryBot.define do
-  factory :task do
+  factory :task, class: Task do
     name { 'default title 1' }
     content { 'default content 1' }
+    deadline { "2022/07/02 18:00" }
   end
 
   factory :second_task, class: Task do
     name { 'default title 2' }
     content { 'default content 2' }
+    deadline { "2022/07/01 18:00" }
+  end
+
+  factory :third_task, class: Task do
+    name { 'default title 3' }
+    content { 'default content 2' }
+    deadline { "2022/07/03 18:00" }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe 'The function of task management', type: :system do
     before do
       FactoryBot.create(:task)
       FactoryBot.create(:second_task)
+      FactoryBot.create(:third_task)
       visit tasks_path
     end
 
@@ -22,14 +23,26 @@ RSpec.describe 'The function of task management', type: :system do
       it 'is shown all created tasks' do
         expect(page).to have_content 'title 1'
         expect(page).to have_content 'title 2'
+        expect(page).to have_content 'title 3'
       end
     end
 
     context 'tasks are arranged in descending created order' do
-      it 'is shown newest task on top' do
+      it 'shows newest task on top' do
         task_list = all('.task_row')
-        expect(task_list[0]). to have_content('title 2')
+        expect(task_list[0]).to have_content('title 3')
+        expect(task_list[1]).to have_content('title 2')
+        expect(task_list[2]).to have_content('title 1')
+      end
+    end
+
+    context 'when the link for sorting by deadline is clicked' do
+      it 'shows that tasks are arranged in descending' do
+        click_link I18n.t('view.sort_link_deadline')
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content('title 3')
         expect(task_list[1]).to have_content('title 1')
+        expect(task_list[2]).to have_content('title 2')
       end
     end
   end
