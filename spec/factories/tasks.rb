51cae07e94d363d3e3a75fa9218a3dbf881c87df
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-29 19:54:40 +0900

    add system test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,17 +3,20 @@ FactoryBot.define do
     name { 'default title 1' }
     content { 'default content 1' }
     deadline { "2022/07/02 18:00" }
+    status { 'not_yet' }
   end
 
   factory :second_task, class: Task do
     name { 'default title 2' }
     content { 'default content 2' }
     deadline { "2022/07/01 18:00" }
+    status { 'doing' }
   end
 
   factory :third_task, class: Task do
     name { 'default title 3' }
     content { 'default content 2' }
     deadline { "2022/07/03 18:00" }
+    status { 'done' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 RSpec.describe 'The function of task management', type: :system do
+
   describe 'the function of to create task' do
     context 'creating task' do
       it 'is shown at index' do
-        task = FactoryBot.create(:task, name: 'task')
+        task = FactoryBot.create(:task)
         visit tasks_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content '未着手'
       end
     end
   end
@@ -53,6 +54,39 @@ RSpec.describe 'The function of task management', type: :system do
         task = FactoryBot.create(:second_task, name: 'aaaaa')
         visit task_path(task)
         expect(page).to have_content 'aaaaa'
+      end
+    end
+  end
+
+  describe 'the function of search' do
+    before do
+      FactoryBot.create(:task)
+      FactoryBot.create(:second_task)
+      FactoryBot.create(:third_task, name: 'aaaaa')
+      visit tasks_path
+    end
+
+    context 'to execute ambiguous search by names' do
+      it "is shown the tasks which include search keyword" do
+        fill_in 'task[name]', with: 'title'
+        click_button I18n.t('view.search_button')
+        expect(page).to have_content 'title 1'
+        expect(page).to have_content 'title 2'
+      end
+    end
+    context 'to execute exact match search by status' do
+      it "is shown the tasks which match exactly" do
+        select "未着手", from: "task[status]"
+        click_button I18n.t('view.search_button')
+        expect(page).to have_content 'title 1'
+      end
+    end
+    context 'to execute ambiguous search by names and exact match search by status' do
+      it "is shown the tasks which include search keyword and the tasks which match exactly" do
+        select "未着手", from: "task[status]"
+        fill_in 'task[name]', with: 'title'
+        click_button I18n.t('view.search_button')
+        expect(page).to have_content 'title 1'
       end
     end
   end
