babfa4ca681f656a57f211d01b5880c1c9749a36
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-30 07:54:19 +0900

    add models test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -23,4 +23,38 @@ describe 'The function of task management', type: :model do
       end
     end
   end
+
+  describe '検索機能' do
+
+    let!(:task) { FactoryBot.create(:task, name: 'task') }
+    let!(:second_task) { FactoryBot.create(:second_task, name: "task 2") }
+    let!(:third_task) { FactoryBot.create(:third_task, name: "sample")}
+
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        expect(Task.search_by_name('task')).to include(task)
+        expect(Task.search_by_name('task')).to include(second_task)
+        expect(Task.search_by_name('task')).not_to include(third_task)
+        expect(Task.search_by_name('task').count).to eq 2
+      end
+    end
+
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.search_by_status('not_yet')).to include(task)
+        expect(Task.search_by_status('not_yet')).not_to include(second_task)
+        expect(Task.search_by_status('not_yet')).not_to include(third_task)
+        expect(Task.search_by_status('not_yet').count).to eq 1
+      end
+    end
+
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        expect(Task.search_by_both('not_yet', 'task')).to include(task)
+        expect(Task.search_by_both('not_yet', 'task')).not_to include(second_task)
+        expect(Task.search_by_both('not_yet', 'task')).not_to include(third_task)
+        expect(Task.search_by_both('not_yet', 'task').count).to eq 1
+      end
+    end
+  end
 end
