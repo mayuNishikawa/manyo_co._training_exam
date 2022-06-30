e64fbdc658739b56f9d66fe306156d9eb8c704dd
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-06-30 14:09:59 +0900

    質問用コミット(再)

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200 }
 
   enum status: { not_yet: 0, doing: 1, done: 2 }
+  enum priority: { high: 0, medium: 1, low: 2 }
 
   scope :search_by_both, ->(pulldown, keyword){ where(status: "#{pulldown}").where('name LIKE ?', "%#{keyword}%") }
   scope :search_by_status, ->(pulldown){ where(status: "#{pulldown}") }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_072709) do
+ActiveRecord::Schema.define(version: 2022_06_29_225754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_072709) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deadline", default: -> { "now()" }, null: false
     t.integer "status", default: 0, null: false
+    t.integer "priority", default: 0, null: false
     t.index ["status", "name"], name: "index_tasks_on_status_and_name"
   end
 
