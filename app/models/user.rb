74fcafd34bc0d0b44f620ae4f14f94c685a39a78
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-07-12 18:23:30 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,11 @@ class User < ApplicationRecord
 
   private
 
-  def admin_cannot_delete
-    throw :abort if User.where(admin: true).count == 1 && self.admin == true
-  end
-
   def admin_cannot_update
     throw :abort if User.where(admin: true).count == 1 && self.admin_change == [true, false]
   end
 
+  def admin_cannot_delete
+    throw :abort if User.where(admin: true).count == 1 && self.admin == true
+  end
 end
